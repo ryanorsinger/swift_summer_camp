e0f25d92f7392d5bb1dc2115a192841c1fff7714
ryanorsinger <ryan.orsinger@gmail.com>
2015-05-14 11:48:23 -0500

    First commit of Day 1 playground

---
+++ CodeupFirstDay.playground/section-1.swift
@@ -0,0 +1,38 @@
+// Playground - noun: a place where people can play
+
+
+// Constants and variables
+// When and why to use constants
+// When and why to use variables
+let myName = "Ryan Orsinger"
+var height = 60.0
+
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+var currentLoginAttempt = 2
+
+// Type Safety
+var someBoolean = true
+someBoolean = false
+someBoolean = true
+someBoolean = "maybe"
+
+
+
+var someOtherBoolean = false;
+
+var someInteger = 23;
+var someDouble = 12.234
+var someFloat = 3.1415926
+
+var arrayOfStrings = [""]
+
+// booleans
+// int
+// float or doubles
+// strings
+
+// collection types
+// Arrays
+// Dictionaries
+// Tuples
